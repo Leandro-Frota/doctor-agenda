683b35bb532936e9f29c7c9e84bf2fc45c7ab3c4
leandro-frota <leandromfrota@gmail.com>
2025-06-12 10:19:03 -0300

    core: add setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="flex gap-0 text-amber-100">Home</h1>
+      <Button>Botcamp!</Button>
     </div>
   );
 }
